7d1ed4c3a23782a7dfdb5f79c4ff80efff09fcb0
MaciejkaG <me@mcjk.cc>
2024-10-27 18:34:55 +0100

    Added some kind of settings animations

--- static/assets/js/services/settings.js
+++ static/assets/js/services/settings.js
@@ -1,5 +1,39 @@
-// Change categories here
-$('#settings .categories span').on('click', function(e) {
-    $('#settings .categories span').removeClass('active');
+function animateContentChange(contentCategoryId) {
+    // Step 1: Animate content out of view
+    anime.remove('#settings .content');
+    anime({
+        targets: '#settings .content',
+        scale: 0.8,
+        opacity: 0,
+        easing: 'easeInOutSine',
+        duration: 250,
+        complete: function () {
+            // Place for content-changing logic
+            // This will be called after the content is out of view
+            // Example:
+            setContentCategory(contentCategoryId);
+
+            // Step 2: Animate content back to original position
+            anime({
+                targets: '#settings .content',
+                scale: [1.2, 1],
+                opacity: 1,
+                easing: 'easeInOutSine',
+                duration: 250,
+            });
+        }
+    });
+}
+
+function setContentCategory(id) {
+    $('#settings .content .category').hide();
+    console.log(`#settings .content .category#${id}`);
+    $(`#settings .content .category#${id}`).show();
+}
+
+// Call the function on category click
+$('#settings .categories .category').on('click', function (e) {
+    animateContentChange($(e.target).data('category-id'));
+    $('#settings .categories .category').removeClass('active');
     $(e.target).addClass('active');
 });
