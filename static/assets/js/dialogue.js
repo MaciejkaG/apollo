e277b5675fa905d605ec7c59fc47d46872e16be2
Maciej Gomola <me@mcjk.cc>
2024-12-05 10:36:41 +0100

    Update dialogues

--- static/assets/js/dialogue.js
+++ static/assets/js/dialogue.js
@@ -3,15 +3,37 @@
     © 2024 77
 */
 
-let dialogueContainer
+let dialogueContainer;
 document.addEventListener('DOMContentLoaded', () => {
     dialogueContainer = document.createElement('div');
     dialogueContainer.classList.add('dialogueContainer');
     document.body.appendChild(dialogueContainer);
 
-    new Dialogue("Wymagana autoryzacja", "Musisz podać hasło, aby Apollo mógł przyłączyć się do sieci Wi-Fi.");
+    dialogueContainer.addEventListener('click', (e) => {
+        if (e.target === dialogueContainer)
+            destroyAllDialogues();
+    });
+
+    const dial = new Dialogue("Wymagana autoryzacja", "Musisz podać hasło, aby Apollo mógł przyłączyć się do sieci Wi-Fi.");
+    dial.setDialogButtons([
+        {
+            type: 'alternative',
+            value: 'Anuluj'
+        },
+        {
+            type: 'default',
+            value: 'Połącz'
+        }
+    ])
+    setTimeout(() => {
+        dial.inTransition(() => {
+            dial.setTitle('Testowy nowy title');
+            dial.setDescription('To jest nowy opis');
+        });
+    }, 3000);
 });
 
+const dialogues = [];
 class Dialogue {
     constructor(title, description) {
         // Create the main container
@@ -19,19 +41,19 @@ class Dialogue {
         this.dialogue.classList.add('dialogue');
 
         // Create the header section
-        const header = document.createElement('div');
-        header.classList.add('header');
+        this.header = document.createElement('div');
+        this.header.classList.add('header');
 
-        const headerTitle = document.createElement('span');
-        headerTitle.classList.add('title');
-        headerTitle.textContent = title;
+        this.title = document.createElement('span');
+        this.title.classList.add('title');
+        this.title.textContent = title;
 
-        const headerDescription = document.createElement('span');
-        headerDescription.classList.add('description');
-        headerDescription.textContent = description;
+        this.description = document.createElement('span');
+        this.description.classList.add('description');
+        this.description.textContent = description;
 
-        header.appendChild(headerTitle);
-        header.appendChild(headerDescription);
+        this.header.appendChild(this.title);
+        this.header.appendChild(this.description);
 
         // Create the content section
         this.content = document.createElement('div');
@@ -42,13 +64,15 @@ class Dialogue {
         this.footer.classList.add('footer');
 
         // Append all sections to the main dialogue container
-        this.dialogue.appendChild(header);
+        this.dialogue.appendChild(this.header);
         this.dialogue.appendChild(this.content);
         this.dialogue.appendChild(this.footer);
 
         // Add the dialogue to the body
         dialogueContainer.appendChild(this.dialogue);
         dialogueContainer.classList.add('active');
+
+        dialogues.push(this);
     }
 
     // This method allows for modifying the dialogue content in a form of a transition.
@@ -56,20 +80,51 @@ class Dialogue {
     inTransition(callback) {
         anime({
             targets: this.dialogue,
-            translateX: [0, 50],
+            translateX: [0, -50],
             opacity: [1, 0],
-            easing: 'easeOutSine',
-            duration: 300,
+            easing: 'easeInSine',
+            duration: 250,
             complete: () => {
                 callback();
                 anime({
                     targets: this.dialogue,
-                    translateX: [-50, 0],
+                    translateX: [50, 0],
                     opacity: [0, 1],
                     easing: 'easeOutSine',
-                    duration: 300,
+                    duration: 250,
                 });
             }
+        });
+    }
+
+    // Sets the title of the dialogue menu
+    setTitle(value) {
+        this.title.innerText = value;
+    }
+
+    // Sets the description of the dialogue menu
+    setDescription(value) {
+        this.description.innerText = value;
+    }
+
+    setDialogButtons(buttons) {
+        buttons.forEach(button => {
+            const btn = document.createElement('button');
+            switch (button.type) {
+                case 'default':
+                    btn.classList.add('default');
+                    break;
+
+                case 'alternative':
+                    btn.classList.add('alternative');
+                    break;
+            
+                default:
+                    break;
+            }
+
+            btn.innerText = button.value;
+            this.footer.appendChild(btn);
         });
     }
 
@@ -80,7 +135,7 @@ class Dialogue {
             translateY: [0, -50],
             opacity: [1, 0],
             easing: 'easeOutSine',
-            duration: 300,
+            duration: 250,
             complete: () => {
                 this.dialogue.remove();
                 if (dialogueContainer.children.length === 0) {
@@ -88,5 +143,13 @@ class Dialogue {
                 }
             }
         });
+
+        delete this;
     }
+}
+
+function destroyAllDialogues() {
+    dialogues.forEach(dialogue => {
+        dialogue.destroy();
+    });
 }
